93897f82d3aea210993522fc146bd4b2c4c30204
User <user@Mac.lan>
2026-05-19 11:29:22 +0100

    Change services layout to 2 columns (2 rows)

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -51,7 +51,7 @@ export const Services = () => {
         </p>
       </motion.div>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid md:grid-cols-2 gap-8">
         {services.map((service, index) => (
           <motion.div
             key={service.title}
